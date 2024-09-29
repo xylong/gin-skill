2ef8f0c6138416e9246e1d31b367fa866f9ccc90
xyl <416319808@qq.com>
2024-09-29 18:15:50 +0800

    token续签

--- app/middlewares/jwt.go
+++ app/middlewares/jwt.go
@@ -6,6 +6,9 @@ import (
 	"gin-skill/global"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"go.uber.org/zap"
+	"strconv"
+	"time"
 )
 
 func JWTAuth(GuardName string) gin.HandlerFunc {
@@ -39,6 +42,23 @@ func JWTAuth(GuardName string) gin.HandlerFunc {
 			response.TokenFail(context)
 			context.Abort()
 			return
+		}
+
+		// token 续签
+		if claims.ExpiresAt-time.Now().Unix() < global.App.Config.Jwt.RefreshGracePeriod {
+			lock := global.Lock("refresh_token_lock", global.App.Config.Jwt.JwtBlacklistGracePeriod)
+			if lock.Get() {
+				err, user := services.JwtService.GetUserInfo(GuardName, claims.Id)
+				if err != nil {
+					zap.L().Error(err.Error())
+					lock.Release()
+				} else {
+					tokenData, _, _ := services.JwtService.CreateToken(GuardName, user)
+					context.Header("new-token", tokenData.AccessToken)
+					context.Header("new-expires-in", strconv.Itoa(tokenData.ExpiresIn))
+					_ = services.JwtService.JoinBlackList(token)
+				}
+			}
 		}
 
 		context.Set("token", token)
--- global/lock.go
+++ global/lock.go
@@ -2,6 +2,9 @@ package global
 
 import (
 	"context"
+	"gin-skill/utils"
+	"github.com/redis/go-redis/v9"
+	"time"
 )
 
 type Interface interface {
@@ -28,43 +31,43 @@ end
 `
 
 // Lock 生成锁
-//func Lock(name string, seconds int64) Interface {
-//	return &lock{
-//		context.Background(),
-//		name,
-//		utils.RandString(16),
-//		seconds,
-//	}
-//}
+func Lock(name string, seconds int64) Interface {
+	return &lock{
+		context.Background(),
+		name,
+		utils.RandString(16),
+		seconds,
+	}
+}
 
 // Get 获取锁
-//func (l *lock) Get() bool {
-//	return App.Redis.SetNX(l.context, l.name, l.owner, time.Duration(l.seconds)*time.Second).Val()
-//}
-//
-//// 阻塞一段时间，尝试获取锁
-//func (l *lock) Block(seconds int64) bool {
-//	starting := time.Now().Unix()
-//	for {
-//		if !l.Get() {
-//			time.Sleep(time.Duration(1) * time.Second)
-//			if time.Now().Unix()-seconds >= starting {
-//				return false
-//			}
-//		} else {
-//			return true
-//		}
-//	}
-//}
-//
-//// 释放锁
-//func (l *lock) Release() bool {
-//	luaScript := redis.NewScript(releaseLockLuaScript)
-//	result := luaScript.Run(l.context, App.Redis, []string{l.name}, l.owner).Val().(int64)
-//	return result != 0
-//}
-//
-//// 强制释放锁
-//func (l *lock) ForceRelease() {
-//	App.Redis.Del(l.context, l.name).Val()
-//}
+func (l *lock) Get() bool {
+	return App.Redis.SetNX(l.context, l.name, l.owner, time.Duration(l.seconds)*time.Second).Val()
+}
+
+// Block 阻塞一段时间，尝试获取锁
+func (l *lock) Block(seconds int64) bool {
+	starting := time.Now().Unix()
+	for {
+		if !l.Get() {
+			time.Sleep(time.Duration(1) * time.Second)
+			if time.Now().Unix()-seconds >= starting {
+				return false
+			}
+		} else {
+			return true
+		}
+	}
+}
+
+// Release 释放锁
+func (l *lock) Release() bool {
+	luaScript := redis.NewScript(releaseLockLuaScript)
+	result := luaScript.Run(l.context, App.Redis, []string{l.name}, l.owner).Val().(int64)
+	return result != 0
+}
+
+// ForceRelease 强制释放锁
+func (l *lock) ForceRelease() {
+	App.Redis.Del(l.context, l.name).Val()
+}
